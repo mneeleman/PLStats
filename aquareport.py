cca99460fccead79f83b79ee31258c6964e3cc32
mneeleman <neeleman@mpia.de>
2024-09-27 16:55:38 -0400

    code to combine dictionaries

--- aquareport.py
+++ aquareport.py
@@ -11,10 +11,10 @@ def load_aquareport(arfile):
 
 def get_projectinfo(ar, mous):
     ps = ar.find('ProjectStructure')
-    mous['proposal_code'] = list(ar.iter('ProposalCode'))[0].text
-    mous['pipeline_recipe'] = list(ar.iter('ProcessingProcedure'))[0].text
-    mous['project_id'] = list(ar.iter('OusEntityId'))[0].text
-    mous['mous_uid'] = list(ar.iter('OusStatusEntityId'))[0].text
-    mous['total_time'] = list(ar.iter('ProcessingTime'))[0].text
-    mous['casa_version'] = list(ar.iter('CasaVersion'))[0].text
-    mous['pipeline_version'] = list(ar.iter('PipelineVersion'))[0].text
+    mous['proposal_code'] = {'value': list(ar.iter('ProposalCode'))[0].text}
+    mous['pipeline_recipe'] = {'value': list(ar.iter('ProcessingProcedure'))[0].text}
+    mous['project_id'] = {'value': list(ar.iter('OusEntityId'))[0].text}
+    mous['mous_uid'] = {'value': list(ar.iter('OusStatusEntityId'))[0].text}
+    mous['total_time'] = {'value': list(ar.iter('ProcessingTime'))[0].text}
+    mous['casa_version'] = {'value': list(ar.iter('CasaVersion'))[0].text}
+    mous['pipeline_version'] = {'value': list(ar.iter('PipelineVersion'))[0].text}
