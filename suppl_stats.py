845f9ce99b77c39b339dfbad7ad32fe6dbec9569
Marcel Neeleman <mneeleman@nrao.edu>
2025-06-24 13:00:37 -0400

    make_suppl_statfile will now exclusively use files in the working directory. default is that it uses the .image files using the casatools:ia

--- suppl_stats.py
+++ suppl_stats.py
@@ -3,6 +3,11 @@ import numpy as np
 import os
 from astropy.io import fits
 import json
+try:
+    a = ia.open()
+    a.done()
+except NameError:
+    from casatools import image as ia
 
 
 def benchmark_make_suppl_statfile(bmdir, outdir='./', overwrite=False):
@@ -16,41 +21,51 @@ def benchmark_make_suppl_statfile(bmdir, outdir='./', overwrite=False):
     projects = list(np.unique([x.split('/')[-2] for x in sorted(glob.glob(bmdir + '/*.*/'))]))
     for pldir in projects:
         print('{0}: {1} of {2}'.format(pldir, projects.index(pldir) + 1, len(projects)))
-        make_suppl_statfile(bmdir + '/' + pldir, jsonfile=outdir + '/' + pldir + '-suppl_stats.json',
-                            overwrite=overwrite)
+        make_suppl_statfile(bmdir + '/' + pldir, overwrite=overwrite, outdir=outdir)
 
 
-def make_suppl_statfile(pldir, jsonfile=None, return_mous=False, overwrite=False):
+def make_suppl_statfile(workingdir, return_mous=False, overwrite=False, outdir=None, use_product_folder=False):
     """
     creates a supplemental stats file in JSON form with additional information that is not prenst in the
     current stats file
-    :param pldir: String pointing to the pipeline run (not the working directory)
-    :param jsonfile: String of the json file that is created. If not set, will use the last part of the pldir string
+    :param workingdir: String pointing to the working directory
     :param return_mous: if set, it will return the dictionary in addition to writing it to file
     :param overwrite: if set, it will overwrite the existing outputfile
+    :param outdir: The directory to put the output into. The default is the current (working directory)
+    :param use_product_folder: if set, will use the fits files in the product foler instead of the .image files
     :return: dictionary of the supplemental stats (optional)
     """
-    if jsonfile is None:
-        plrun_name = pldir.split('/')[-1]
-        jsonfile = plrun_name + '-suppl_stats.json'
-    if os.path.exists(jsonfile) and overwrite is False:
-        print('make_suppl_statfile: file: {} already exists will not overwrite it'.format(jsonfile))
+    # define the naming of the suppl. stats file. This is done first, to see if the file exists, and if so,
+    # the function does not need to be run.
+    workingdir = workingdir + '/' if workingdir[-1] != '/' else workingdir
+    outdir = workingdir if outdir is None else outdir
+    outdir = outdir + '/' if outdir[-1] != '/' else outdir
+    try:  # will try to create a more meaningful name, if this fails, will default to non-descriptive name
+        mousname = glob.glob(workingdir + '/pipeline_stats_*.json')[0].split('/')[-1][15:-5]
+        timestamp = glob.glob(workingdir + '/pipeline-*T*.timetracker.json')[-1].split('/')[-1].split('.')[0][9:]
+        jsonfile = 'pipeline-suppl_stats-' + mousname + '-' + timestamp + '.json'
+    except NameError:
+        jsonfile = 'pipeline-suppl_stats.json'
+    if os.path.exists(outdir + jsonfile) and overwrite is False:
+        print('make_suppl_statfile: file: {} already exists will not overwrite it'.format(outdir + jsonfile))
         return
+    # create the dictionary
     mous = {'EB': {}, 'TARGET': {}}
-    scrape_flagfiles(mous, pldir)
-    im_list, image_path = __get_imagelist__(pldir)
+    scrape_flagfiles(mous, workingdir)
+    im_list, image_path = __get_imagelist__(workingdir, use_product_folder=use_product_folder)
     for image in im_list:
         get_imagestats(mous, image_path + image)
-    with open(jsonfile, 'w') as fp:
+    # output the file and optionally return the dictionary
+    with open(outdir + jsonfile, 'w') as fp:
         json.dump(mous, fp)
     if return_mous:
         return mous
 
 
-def scrape_flagfiles(mous, pldir):
-    flag_files = glob.glob('{}/working/*.flagtemplate.txt'.format(pldir))
+def scrape_flagfiles(mous, workingdir):
+    flag_files = glob.glob('{}*.flagtemplate.txt'.format(workingdir))
     if not flag_files:
-        print('__scrape_flagfiles__: no flagging files in working directory')
+        print('__scrape_flagfiles__: no flagging files in working directory: {}'.format(workingdir))
         return
     for ff in flag_files:
         eb = ff.split('/')[-1].split('.')[0] + '.ms'
@@ -65,26 +80,24 @@ def scrape_flagfiles(mous, pldir):
 
 
 def get_imagestats(mous, image):
-    usefits = True if image[-4:] == 'fits' else False
-    header, im, im_pbcor, im_pb, im_mask = __load_images__(image, use_fits=usefits)
-    header.remove('HISTORY', remove_all=True)
-    if header['OBJECT'] not in mous['TARGET']:
+    header, im, im_pbcor, im_pb, im_mask = __load_images__(image)
+    if header['OBJECT'].rstrip() not in mous['TARGET']:
         mous['TARGET'][header['OBJECT']] = {}
     im_rms, im_mad = __get_rms__(im, im_pb, im_mask)
     im_max = __get_max__(im)
-    if header['SPW'] not in mous['TARGET'][header['OBJECT']]:
+    if header['SPW'].rstrip() not in mous['TARGET'][header['OBJECT']]:
         mous['TARGET'][header['OBJECT']][header['SPW']] = {}
     t_im = mous['TARGET'][header['OBJECT']][header['SPW']]
-    t_im['makeimages_science_' + header['SPECMODE'] + '_bmaj'] = {'value': header['BMAJ'] * 3600}
-    t_im['makeimages_science_' + header['SPECMODE'] + '_bmin'] = {'value': header['BMIN'] * 3600}
-    t_im['makeimages_science_' + header['SPECMODE'] + '_bpa'] = {'value': header['BPA']}
-    t_im['makeimages_science_' + header['SPECMODE'] + '_rms'] = {'value': im_rms}
-    t_im['makeimages_science_' + header['SPECMODE'] + '_mad'] = {'value': im_mad}
-    t_im['makeimages_science_' + header['SPECMODE'] + '_max'] = {'value': im_max}
+    t_im['makeimages_science_' + header['SPECMODE'].rstrip() + '_bmaj'] = {'value': header['BMAJ'] * 3600}
+    t_im['makeimages_science_' + header['SPECMODE'].rstrip() + '_bmin'] = {'value': header['BMIN'] * 3600}
+    t_im['makeimages_science_' + header['SPECMODE'].rstrip() + '_bpa'] = {'value': header['BPA']}
+    t_im['makeimages_science_' + header['SPECMODE'].rstrip() + '_rms'] = {'value': im_rms}
+    t_im['makeimages_science_' + header['SPECMODE'].rstrip() + '_mad'] = {'value': im_mad}
+    t_im['makeimages_science_' + header['SPECMODE'].rstrip() + '_max'] = {'value': im_max}
 
 
-def __load_images__(image, use_fits=True):
-    if use_fits:
+def __load_images__(image):
+    if image[-5:] == '.fits':
         hdu = fits.open(image)
         header = hdu[0].header
         im_pbcor = np.squeeze(hdu[0].data)
@@ -99,19 +112,39 @@ def __load_images__(image, use_fits=True):
             im_mask = np.zeros_like(im_pbcor).astype(bool)
         im = im_pbcor * im_pb
     else:
-        raise NotImplementedError('Need to implement image load from .image files to bypass the use of fits')
+        casa_im = ia().newimagefromfile(image)
+        header = casa_im.fitsheader()
+        im = np.squeeze(casa_im.getchunk())
+        casa_im.done()
+        casa_im = ia().newimagefromfile(image + '.pbcor')
+        im_pbcor = np.squeeze(casa_im.getchunk())
+        casa_im.done()
+        casa_im = ia().newimagefromfile(image.replace('image', 'pb'))
+        im_pb = np.squeeze(casa_im.getchunk())
+        casa_im.done()
+        if os.path.exists(image.replace('image', 'mask')):
+            casa_im = ia().newimagefromfile(image.replace('image', 'mask'))
+            im_mask = np.squeeze(casa_im.getchunk()).astype(bool)
+            casa_im.done()
+        else:
+            im_mask = np.zeros_like(im).astype(bool)
     return header, im, im_pbcor, im_pb, im_mask
 
 
-def __get_imagelist__(pldir):
-    imlist = glob.glob(pldir + '/S*/G*/M*/products/*_sci*.pbcor.fits')
-    if not imlist:
-        print('__get_imagelist__: no images in {}'.format(pldir + '/S*/G*/M*/products/*_sci*.pbcor.fits'))
-        image_list = []
-        return image_list, ''
+def __get_imagelist__(workingdir, use_product_folder=False):
+    if use_product_folder:
+        imlist = glob.glob(workingdir + '../products/*_sci*.pbcor.fits')
+        if not imlist:
+            print('__get_imagelist__: no images in {}'.format(workingdir + '../products/*_sci*.pbcor.fits'))
+            image_list = []
+            return image_list, ''
+        else:
+            image_list = [x.split('/')[-1] for x in imlist if 'tt1.pbcor.fits' not in x]
+            return image_list, '/'.join(imlist[0].split('/')[:-1]) + '/'
     else:
-        image_list = [x.split('/')[-1] for x in imlist if 'tt1.pbcor.fits' not in x]
-    return image_list, '/'.join(imlist[0].split('/')[:-1]) + '/'
+        imlist = glob.glob(workingdir + '*_sci*.image')
+        image_list = [x.split('/')[-1] for x in imlist]
+        return image_list, workingdir
 
 
 def __get_rms__(im, im_pb, im_mask):
